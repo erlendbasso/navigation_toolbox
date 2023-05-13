d9c3e0f0b71077929d6e38de7117b27b5a441865
Erlend Andreas Basso <erlend.a.basso@ntnu.no>
2023-05-13 12:02:12 +0200

    Should compile with no_std

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
-// #![no_std]
-#![cfg_attr(feature = "no_std", no_std)]
+#![no_std]
+// #![cfg_attr(feature = "no_std", no_std)]
 
-#[cfg(feature = "no_std")]
-extern crate core as std;
+// #[cfg(feature = "no_std")]
+// extern crate core as std;
+use libm::{sqrt, sin, cos, pow, tan};
 
 extern crate nalgebra as na;
 use na::{SMatrix, Vector3, UnitQuaternion, Quaternion, Matrix3, Rotation3};
 
-use approx::assert_relative_eq;
+// use approx::assert_relative_eq;
 
 pub struct Ellipsoid {
     a: f64,
@@ -28,11 +29,12 @@ pub fn lat_lon_height_to_ecef(lat: f64, lon: f64, height: f64) -> Vector3<f64> {
     let a = 6378137.0;
     let e = 0.08181919;
 
-    let radius_n = a / f64::sqrt( 1.0 - f64::powi(e, 2) * f64::powi(f64::sin(lat), 2) );
+
+    let radius_n = a / sqrt( 1.0 - pow(e, 2.0) * pow(sin(lat), 2.0) );
     Vector3::new(
-        (radius_n + height) * f64::cos(lat) * f64::cos(lon),
-        (radius_n + height) * f64::cos(lat) * f64::sin(lon),
-        (radius_n * ( 1.0 - f64::powi(e, 2) ) + height) * f64::sin(lat)
+        (radius_n + height) * cos(lat) * cos(lon),
+        (radius_n + height) * cos(lat) * sin(lon),
+        (radius_n * ( 1.0 - pow(e, 2.0) ) + height) * sin(lat)
     )
 }
 
@@ -40,13 +42,13 @@ pub fn omega_en_n(lat: f64, height: f64, vel_eb_n: Vector3<f64>) -> Vector3<f64>
     let a = WGS84.a;
     let e = 0.08181919;
 
-    let radius_e = a / f64::sqrt( 1.0 - f64::powi(e, 2) * f64::powi(f64::sin(lat), 2) );
-    let radius_n = a * (1.0 - f64::powi(e, 2)) / f64::powf( 1.0 - f64::powi(e, 2) * f64::powi(f64::sin(lat), 2), 1.5);
+    let radius_e = a / sqrt( 1.0 - pow(e, 2.0) * pow(sin(lat), 2.0) );
+    let radius_n = a * (1.0 - pow(e, 2.0)) / pow( 1.0 - pow(e, 2.0) * pow(sin(lat), 2.0), 1.5);
 
     Vector3::new(
       vel_eb_n[1] / (radius_e + height),
       - vel_eb_n[0] / (radius_n + height),
-      -vel_eb_n[1] * f64::tan(lat) / (radius_e + height)
+      -vel_eb_n[1] * tan(lat) / (radius_e + height)
     )
 }
 
@@ -58,14 +60,14 @@ pub fn gravity_ecef(r_eb_e: &Vector3<f64>) -> Vector3<f64> {
 
     let r_norm = r_eb_e.norm();
 
-    let z_scale = 5.0 * f64::powi(r_eb_e[2] / r_norm, 2);
-    let gamma = - mu / f64::powi(r_norm, 3) * (r_eb_e + 1.5 * J_2 
-        * f64::powi(a / r_norm, 2) 
+    let z_scale = 5.0 * pow(r_eb_e[2] / r_norm, 2.0);
+    let gamma = - mu / pow(r_norm, 3.0) * (r_eb_e + 1.5 * J_2 
+        * pow(a / r_norm, 2.0) 
         *  Vector3::new((1.0 - z_scale ) * r_eb_e[0], 
                         (1.0 - z_scale) * r_eb_e[1], 
                         (3.0 - z_scale) * r_eb_e[2]));
 
-    gamma + f64::powi(omega_ie, 2) * Matrix3::from_diagonal(&Vector3::new(1.0, 1.0, 0.0)) * r_eb_e
+    gamma + pow(omega_ie, 2.0) * Matrix3::from_diagonal(&Vector3::new(1.0, 1.0, 0.0)) * r_eb_e
 }
 
 pub fn gravity_ned(lat: f64, lon: f64, height: f64) -> Vector3<f64> {
@@ -73,9 +75,9 @@ pub fn gravity_ned(lat: f64, lon: f64, height: f64) -> Vector3<f64> {
     let g_e = gravity_ecef(&r_eb_e);
     
     let rot_ned_ecef = Matrix3::new(
-        -f64::sin(lat) * f64::cos(lon), -f64::sin(lat) * f64::sin(lon), f64::cos(lat),
-        -f64::sin(lon), f64::cos(lon), 0.0,
-        -f64::cos(lat) * f64::cos(lon), -f64::cos(lat) * f64::sin(lon), -f64::sin(lat)
+        -sin(lat) * cos(lon), -sin(lat) * sin(lon), cos(lat),
+        -sin(lon), cos(lon), 0.0,
+        -cos(lat) * cos(lon), -cos(lat) * sin(lon), -sin(lat)
     );
 
     rot_ned_ecef * g_e
@@ -83,9 +85,9 @@ pub fn gravity_ned(lat: f64, lon: f64, height: f64) -> Vector3<f64> {
 
 pub fn quat_ned_ecef(lat: f64, lon: f64) -> UnitQuaternion<f64> {
     let rot_ned_ecef = Matrix3::new(
-        -f64::sin(lat) * f64::cos(lon), -f64::sin(lat) * f64::sin(lon), f64::cos(lat),
-        -f64::sin(lon), f64::cos(lon), 0.0,
-        -f64::cos(lat) * f64::cos(lon), -f64::cos(lat) * f64::sin(lon), -f64::sin(lat)
+        -sin(lat) * cos(lon), -sin(lat) * sin(lon), cos(lat),
+        -sin(lon), cos(lon), 0.0,
+        -cos(lat) * cos(lon), -cos(lat) * sin(lon), -sin(lat)
     );
 
     UnitQuaternion::from_rotation_matrix(&Rotation3::from_matrix_unchecked(rot_ned_ecef))
@@ -93,17 +95,17 @@ pub fn quat_ned_ecef(lat: f64, lon: f64) -> UnitQuaternion<f64> {
 
 pub fn rot_ned_ecef(lat: f64, lon: f64) -> Matrix3<f64> {
     Matrix3::new(
-        -f64::sin(lat) * f64::cos(lon), -f64::sin(lat) * f64::sin(lon), f64::cos(lat),
-        -f64::sin(lon), f64::cos(lon), 0.0,
-        -f64::cos(lat) * f64::cos(lon), -f64::cos(lat) * f64::sin(lon), -f64::sin(lat)
+        -sin(lat) * cos(lon), -sin(lat) * sin(lon), cos(lat),
+        -sin(lon), cos(lon), 0.0,
+        -cos(lat) * cos(lon), -cos(lat) * sin(lon), -sin(lat)
     )
 }
 
 pub fn rot_ecef_ned(lat: f64, lon: f64) -> Matrix3<f64> {
     Matrix3::new(
-        -f64::sin(lat) * f64::cos(lon), -f64::sin(lon), -f64::cos(lat) * f64::cos(lon),
-        -f64::sin(lat) * f64::sin(lon), f64::cos(lon), -f64::cos(lat) * f64::sin(lon),
-        f64::cos(lat), 0.0, -f64::sin(lat)
+        -sin(lat) * cos(lon), -sin(lon), -cos(lat) * cos(lon),
+        -sin(lat) * sin(lon), cos(lon), -cos(lat) * sin(lon),
+        cos(lat), 0.0, -sin(lat)
     )
 }
 
@@ -121,14 +123,14 @@ pub fn lat_height_to_grav_accel_ned(lat: f64, height: f64) -> Vector3<f64> {
     let omega_ie = 7.292115E-5; // Earth rotation rate (rad/s)
 
     // Calculate surface gravity using the Somigliana model, (2.134)
-    let sin_lat_squared = f64::sin(lat) * f64::sin(lat);
-    let g_0 = 9.7803253359 * (1.0 + 0.001931853 * sin_lat_squared) / f64::sqrt(1.0 - e * e * sin_lat_squared);
+    let sin_lat_squared = sin(lat) * sin(lat);
+    let g_0 = 9.7803253359 * (1.0 + 0.001931853 * sin_lat_squared) / sqrt(1.0 - e * e * sin_lat_squared);
 
     Vector3::new(
-        -8.08e-9 * height * f64::sin(2.0 * lat), // Calculate north gravity using (2.140)
+        -8.08e-9 * height * sin(2.0 * lat), // Calculate north gravity using (2.140)
         0.0, // East gravity is zero
         g_0 * (1.0 - (2.0 / radius_wgs84) * (1.0 + f * (1.0 - 2.0 * sin_lat_squared) +
-        (f64::powi(omega_ie, 2) * f64::powi(radius_wgs84, 2) * b / mu)) * height + (3.0 * f64::powi(height, 2) / f64::powi(radius_wgs84, 2)))   // Calculate down gravity using (2.139)
+        (pow(omega_ie, 2.0) * pow(radius_wgs84, 2.0) * b / mu)) * height + (3.0 * pow(height, 2.0) / pow(radius_wgs84, 2.0)))   // Calculate down gravity using (2.139)
     )
     }
 
@@ -136,6 +138,7 @@ pub fn lat_height_to_grav_accel_ned(lat: f64, height: f64) -> Vector3<f64> {
 #[cfg(test)]
 mod tests {
     use core::f64::consts::PI;
+    use approx::assert_relative_eq;
 
     use super::*;
 
